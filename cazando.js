76661a271334cec9a8ec84409182cdd8b302b1ad
gianlucaparedes6-sys <gianlucaparedes6@gmail.com>
2026-04-12 08:55:50 -0500

    agregar funcion graficarRectangulo

--- cazando.js
+++ cazando.js
@@ -5,25 +5,21 @@ const ALTURA_GATO = 60;
 const ANCHO_GATO = 40;
 const ANCHO_COMIDA = 20;
 const ALTURA_COMIDA = 20;
-
 let gatoX = canvas.width / 2 - ANCHO_GATO / 2;
 let gatoY = canvas.height /2 - ALTURA_GATO / 2;
 let comidaX = 480;
 let comidaY = 480;
-
-
 function iniciarJuego() {
-   dibujarComida();
-    dibujarGato();
-
+   graficarComida();
+    graficarGato();
 }
-
-
-function dibujarGato() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(gatoX , gatoY , ANCHO_GATO, ALTURA_GATO);
-}
-function dibujarComida() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(comidaX , comidaY , ANCHO_COMIDA, ALTURA_COMIDA);
+function graficarGato() {
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTURA_GATO, "black");
+   }
+function graficarComida() {
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTURA_COMIDA, "red");
+   }
+function graficarRectangulo(x, y, ancho, altura, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, altura);
 }
