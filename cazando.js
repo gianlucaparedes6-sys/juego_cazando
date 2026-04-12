88236e64ad90d9f9ce0511cd3774ce20355b6c28
gianlucaparedes6-sys <gianlucaparedes6@gmail.com>
2026-04-12 13:17:18 -0500

    crecion posicion aleatoria y contador de puntos

--- cazando.js
+++ cazando.js
@@ -7,8 +7,10 @@ const ANCHO_COMIDA = 20;
 const ALTURA_COMIDA = 20;
 let gatoX = canvas.width / 2 - ANCHO_GATO / 2;
 let gatoY = canvas.height /2 - ALTURA_GATO / 2;
-let comidaX = 480;
-let comidaY = 480;
+let comidaX = Math.floor(Math.random() * 20) * 20; 
+let comidaY = Math.floor(Math.random() * 20) * 20;
+let puntaje = 0;
+
 function iniciarJuego() {
    graficarComida();
     graficarGato();
@@ -22,6 +24,8 @@ function graficarComida() {
 function graficarRectangulo(x, y, ancho, altura, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, ancho, altura);
+    
+
 }
 function limpiarCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -31,7 +35,7 @@ function moverIzquierda() {
     limpiarCanvas();
     graficarComida();
     graficarGato();
-    detectarColision();
+    detectarColision();;
 }
 function moverDerecha() {
     gatoX = gatoX +10;
@@ -53,9 +57,30 @@ function moverAbajo() {
     graficarComida();
     graficarGato();
     detectarColision();
-}
+ }
 function detectarColision() {
     if (comidaX + ANCHO_COMIDA > gatoX && comidaX < gatoX + ANCHO_GATO && comidaY + ALTURA_COMIDA > gatoY && comidaY < gatoY + ALTURA_GATO) {
-        alert("COMIDA ATRAPADA");
+        //alert("COMIDA ATRAPADA");
+        aparecerComida();
+        puntaje = puntaje + 1;
+        let componente=document.getElementById("puntos");
+        componente.textContent=puntaje;
+                
     }
+}
+function generarAleatorio(min,max){
+    let random=Math.random();
+    let numero=random*(max-min+1);
+    let numeroEntero = Math.ceil(numero);
+    numeroEntero = numeroEntero+min-1;
+    return numeroEntero
+}
+function aparecerComida() {
+    comidaX=generarAleatorio(0,canvas.width - ANCHO_COMIDA);
+    comidaY=generarAleatorio(0,canvas.height - ALTURA_COMIDA);
+}
+
+function mostrarEnSpan(idSpan,valor){
+    let componente=document.getElementById(idSpan);
+    componente.textContent=valor;
 }
